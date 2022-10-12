268060bad8bb2e370e3e5f36a482955fd400af6c
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-12 12:55:47 +0300

    added changes to the solution of problem 3

--- Третий проект/Program.cs
+++ Третий проект/Program.cs
@@ -1,3 +1,8 @@
-﻿int number1 = 3;
-int number2 = 5;
+﻿int number1 = new Random().Next(1, 10);
+Console.Write("Первое случайное число ");
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.Write("Второе случайное число ");
+Console.WriteLine(number2);
+Console.Write("Результат суммы двух случайных чисел равен ");
 Console.WriteLine(number1 + number2);
