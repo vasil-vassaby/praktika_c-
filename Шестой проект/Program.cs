a62d35be58debed20155e58230affbcbd35f7f3d
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-12 13:18:26 +0300

    added a folder Шестой проект

---
+++ Шестой проект/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 100;
+int b = 23;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
