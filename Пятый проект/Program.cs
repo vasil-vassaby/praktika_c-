30675ad201149e8268a3c237f1d45b2a5b266913
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-12 13:10:31 +0300

    added a folder Пятый проект

---
+++ Пятый проект/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") //перевод символов в нижний регистр 
+{
+    Console.Write("Ура, это же Маша пришла!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
