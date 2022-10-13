bdd61edc47a0772b6ae5e10b7622ea70f3503bb5
VasilAshrafzyanov <vasil-vassaby@yandex.ru>
2022-10-13 06:13:16 +0300

    added a folder Седьмой проект

---
+++ Седьмой проект/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(5, 1);
+//Console.WriteLine("0");
+
+int xa = 1, ya = 5;
+int xb = 1, yb = 40; 
+int xc = 80, yc = 40;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("a");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("b");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("c");
